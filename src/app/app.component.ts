3ef0c31d6076abf0a345810f72ae5d6cca435d05
Judith Portocarrero <jportocarrec@ransa.net>
2025-02-26 00:38:05 -0500

    conf inicial angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'bcl-front';
+
+  // Import the functions you need from the SDKs you need
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig = {
+    apiKey: "AIzaSyCZkDITXdoP66V7pZDbnZyf69x3xkuvpRI",
+    authDomain: "bcl-dev-92308.firebaseapp.com",
+    projectId: "bcl-dev-92308",
+    storageBucket: "bcl-dev-92308.firebasestorage.app",
+    messagingSenderId: "961121153987",
+    appId: "1:961121153987:web:a42812a707afadad6d4376",
+    measurementId: "G-TS785WSPF3"
+  };
+
+  // Initialize Firebase
+  app = initializeApp(this.firebaseConfig);
+  // analytics = getAnalytics(this.app);
+}
